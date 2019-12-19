d85680f4ffc044c80bafcce7497f1edb765e906e
matthew-charles-chan <matthew.charles.chan@gmail.com>
2019-12-19 22:32:08 +0000

    iterating over arrays to print all elements

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("stuff to bring:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i])
+}
+
